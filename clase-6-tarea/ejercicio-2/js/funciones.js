960bd9a217004276c429ee064c13b8b936c81e52
Iara Pou <iarapou21@hotmail.com>
2022-10-26 19:06:58 -0300

    Crea función validarSalario
    Chequea que no esté vacío, que sea mayor a 0 y que sea un número entero.

--- clase-6-tarea/ejercicio-2/js/funciones.js
+++ clase-6-tarea/ejercicio-2/js/funciones.js
@@ -34,3 +34,19 @@ function devolverMayor(array) {
     }
     return numeroMayor;
 }
+
+function validarSalario(salario){
+    if(salario===""){
+        return "Todos los campos deben estar completos.";
+    }
+
+    if(salario<=0){
+        return "El campo debe ser mayor a 0.";
+    }
+
+    if(!/^[0-9]$/.test(Math.trunc(salario))){
+        return "El campo debe ser un número.";
+    }
+
+    return "";
+}
