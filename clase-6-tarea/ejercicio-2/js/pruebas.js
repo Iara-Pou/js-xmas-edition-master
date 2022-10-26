ee59cab4b7c50ab1279daf1b63877737e9580873
Iara Pou <iarapou21@hotmail.com>
2022-10-26 20:39:05 -0300

    Crea pruebas para validarSalario
    También formatee el js.

--- clase-6-tarea/ejercicio-2/js/pruebas.js
+++ clase-6-tarea/ejercicio-2/js/pruebas.js
@@ -1,29 +1,48 @@
-function pruebaDevolverMayor(){
+function pruebaDevolverMayor() {
     console.assert(
-        devolverMayor([1,2,3]) === 3, "Devolver mayor no está devolviendo el numero mayor."
+        devolverMayor([1, 2, 3]) === 3, "Devolver mayor no está devolviendo el numero mayor."
     )
 }
 
-function pruebaDevolverMenor(){
-console.assert(
-    devolverMenor([1,2,3]) === 1, "Devolver menor no está devolviendo el menor."
-)    
-}
-
-function pruebaDevolverPromedio(){
-console.assert(
-    calcularPromedio([2,2,2]) === 2, "Calcular promedio no está retornando el promedio del array."
-)    
-}
-
-function pruebaDevolverPromedioMensual(){
+function pruebaDevolverMenor() {
     console.assert(
-        calcularPromedioMensual([2,2,2]) === 2/12, "Calcular promedio no está retornando el promedio mensual del array."
-    )    
+        devolverMenor([1, 2, 3]) === 1, "Devolver menor no está devolviendo el menor."
+    )
+}
+
+function pruebaDevolverPromedio() {
+    console.assert(
+        calcularPromedio([2, 2, 2]) === 2, "Calcular promedio no está retornando el promedio del array."
+    )
+}
+
+function pruebaDevolverPromedioMensual() {
+    console.assert(
+        calcularPromedioMensual([2, 2, 2]) === 2 / 12, "Calcular promedio no está retornando el promedio mensual del array."
+    )
+}
+
+function pruebaValidarSalario() {
+    console.assert(
+        validarSalario(-10) === "El campo debe ser mayor a 0.", "Validar salario no está validando que el campo sea mayor a 0."
+    )
+
+    console.assert(
+        validarSalario("aaa") === "El campo debe ser un número.", "Validar salario no está validando que el campo sea un número."
+    )
+
+    console.assert(
+        validarSalario("") === "Todos los campos deben estar completos.", "Validar salario no está validando que el campo esté completo."
+    )
+
+    console.assert(
+        validarSalario(12) === "", "Validar salario no está validando un input correcto."
+    )
 }
 
 pruebaDevolverMayor();
 pruebaDevolverMenor();
 pruebaDevolverPromedio();
 pruebaDevolverPromedioMensual();
+pruebaValidarSalario();
 
