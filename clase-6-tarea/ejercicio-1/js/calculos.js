07cb017b4107c8592a56af98db4d49e09d42ba84
Iara Pou <iarapou21@hotmail.com>
2022-11-01 20:17:38 -0300

    Crea archivo Js para validaciones
    Antes estaba en el archivo cálculos, pero no era un cálculo.

--- clase-6-tarea/ejercicio-1/js/calculos.js
+++ clase-6-tarea/ejercicio-1/js/calculos.js
@@ -8,7 +8,7 @@ function devolverPromedio(array) {
 
 function devolverNumeroMenor(numeros) {
     let numeroMenor = numeros[0];
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < numeros.length; i++) {
         if (numeros[i] < numeroMenor) {
             numeroMenor = Number(numeros[i]);
         }
@@ -26,32 +26,3 @@ function devolverNumeroMayor(numeros) {
     return numeroMayor;
 }
 
-function validarCantidadIntegrantes(cantidad){
-    
-    if (cantidad === ""){
-        return "Debe ingresar por lo menos 1 carácter.";
-    }
-
-    if(cantidad < 0){
-        return "La cantidad debe ser mayor a cero."
-    }
-
-    if(! /^[0-9]+$/.test(cantidad)){
-        return "La cantidad debe ser un número entero."
-    }
-
-    return "";
-}
-
-function validarEdadIntegrante(edad){
-    
-    if (edad <= 0){
-        return "Todos los campos deben tener un numero positivo ingresado."
-    }
-
-    if(! /^[0-9]+$/.test(edad)){
-        return "El campo debe tener un número entero ingresado."
-    }
-
-    return "";
-}
