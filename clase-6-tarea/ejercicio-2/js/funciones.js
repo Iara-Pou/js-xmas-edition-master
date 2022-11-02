d2b221d0e2c9451772f59843d21e124302c31a1b
Iara Pou <iarapou21@hotmail.com>
2022-11-02 19:36:09 -0300

    Linkea funciones de ejercicio-1
    Para evitar repetir funciones innecesariamente.

--- clase-6-tarea/ejercicio-2/js/funciones.js
+++ clase-6-tarea/ejercicio-2/js/funciones.js
@@ -1,39 +1,6 @@
-function calcularPromedio(numeros) {
-    let totalSuma = 0;
-    let contador= 0;
-    
-    for (let i = 0; i < numeros.length; i++) {
-        if(numeros[i]!==0){
-            totalSuma += numeros[i];
-            contador++;
-        }
-    }
-    return totalSuma / contador;
-}
-
 function calcularPromedioMensual(array) {
     const MESES_EN_UN_ANIO = 12;
-    return calcularPromedio(array) / MESES_EN_UN_ANIO;
-}
-
-function devolverNumeroMenor(numeros) {
-    let numeroMenor = numeros[0];
-    for (let i = 0; i < numeros.length; i++) {
-        if (numeros[i] < numeroMenor) {
-            numeroMenor = numeros[i];
-        }
-    }
-    return numeroMenor;
-}
-
-function devolverNumeroMayor(numeros) {
-    let numeroMayor = numeros[0];
-    for (let i = 0; i < numeros.length; i++) {
-        if (numeros[i] > numeroMayor) {
-            numeroMayor = numeros[i];
-        }
-    }
-    return numeroMayor;
+    return devolverPromedio(array) / MESES_EN_UN_ANIO;
 }
 
 function validarSalario(salario){
--- clase-6-tarea/ejercicio-2/js/pruebas.js
+++ clase-6-tarea/ejercicio-2/js/pruebas.js
@@ -12,7 +12,7 @@ function pruebaDevolverNumeroMenor() {
 
 function pruebaDevolverPromedio() {
     console.assert(
-        calcularPromedio([2, 2, 2]) === 2, "Calcular promedio no está retornando el promedio del array."
+        devolverPromedio([2, 2, 2]) === 2, "Calcular promedio no está retornando el promedio del array."
     )
 }
 
